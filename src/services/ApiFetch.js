e8e6977d01c884b9c6acb8b56002da51f0404bc5
Angel Huayas <angelhuayas@gmail.com>
2021-12-02 22:12:05 -0500

    add request function script

---
+++ src/services/ApiFetch.js
@@ -0,0 +1,9 @@
+const baseUrl = "https://hn.algolia.com/api/v1/"
+
+const apiFetch = async (query, page)=>{
+
+  const data = await fetch(`${baseUrl}search_by_date?query=${query}&page=${page}`)
+  return  data.json()
+}
+
+export default apiFetch;
